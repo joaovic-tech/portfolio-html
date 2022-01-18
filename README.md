f5b91052583f3cf14a41da2ef1e87a12b14dcd3f
João Victor <joaovictorca2004@gmail.com>
2022-01-18 19:33:00 -0300

    pushVercel

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿Projeto utilizado usando Node.js
+Comando de inicio
+### npm init
+
+Dependencias e comandos instaladas
+### npm install express
+### npm install ejs
+### npm install -g nodemon
+### npm install --save-dev nodemon
---
+++ public/js/index.js
@@ -0,0 +1,49 @@
+/* animação das imagens */
+function ativaNoScroll(){
+    document.querySelectorAll('.imgALL').forEach((img, index) => {
+        if(img.getBoundingClientRect().top < (window.innerHeight) / 2){
+            img.src = img.getAttribute('data-src');
+        }
+    })
+}
+
+window.addEventListener('scroll', ativaNoScroll);
+
+/* animaçoes geral */
+
+const debounce = function(func, wait, immediate){
+    let timeout;
+    return function(...args){
+        const context = this;
+        const later = function(){
+            timeout = null;
+            if(!immediate) func.apply(context, args);
+        };
+        const callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if(callNow)func.apply(context, args);
+    };
+};
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3 / 4));
+    target.forEach(function(e){
+        if((windowTop) > e.offsetTop){
+            e.classList.add(animationClass);
+        }else{
+            e.classList.remove(animationClass);
+        }
+    })
+}
+
+animeScroll();
+
+if(target.length){
+    window.addEventListener('scroll', debounce(function(){
+        animeScroll();
+    }, 200));
+}
